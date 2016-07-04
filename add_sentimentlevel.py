411d158883417107d52d6997b4f391c955f54170
oktavianidewi <oktaviani.dewi@gmail.com>
2016-07-04 11:14:36 +0900

    sentiment level label ok

--- add_sentimentlevel.py
+++ add_sentimentlevel.py
@@ -2,33 +2,26 @@ import csv
 import os
 
 new_row = []
-filename = '1allpost_subj_pol.csv'
-data_file = open(filename, 'rb')
-csv_data = csv.reader(data_file, lineterminator = '\n')
+data_file = open('1allpost_subj_pol.csv', 'rb')
+csv_read = csv.reader(data_file, delimiter=',')
 
-data_write = open(filename, 'wb')
-# csv_write = csv.writer(data_write, delimiter=',')
+data_write = open('1allpost_subj_pol_sentlev.csv', 'wb')
 csv_write = csv.writer(data_write, lineterminator = '\n')
-csv_write.writerow(next(csv_data))
-csv_write.writerow(new_row)
 
-for i, x in enumerate(csv_data):
-    # print x
-    # header = next(csv_data)
-    new_row.append('SenLev')
-    # if i == 0 :
-    #    print x
+all = []
+row = next(csv_read)
+row.append('SenLev')
+all.append(row)
 
+for i, row in enumerate(csv_read):
     if i > 0 :
-        subj_val = float(x[5])
-        pol_val = float(x[7])
-        print subj_val
-        print pol_val
-        if (subj_val < 0.0) and ( pol_val < 0.0):
+        subj_val = float(row[5])
+        pol_val = float(row[7])
+        if (subj_val < 0.0) and (pol_val < 0.0):
             status = 'SenLev-1'
-        elif (subj_val == 0.0) and ( pol_val == 0.0):
+        elif (subj_val == 0.0) and (pol_val == 0.0):
             status = 'SenLev0'
-        elif ( 0.0 < subj_val <= 0.567) and ( 0.0 < pol_val < 0.481):
+        elif ( 0.0 < subj_val <= 0.567) and (0.0 < pol_val < 0.481):
             status = 'SenLev1'
         elif ( 0.567 < subj_val <= 0.752 and pol_val <= 0.850 ) or ( 0.481 < pol_val < 0.850 and subj_val <= 0.752 ):
             status = 'SenLev2'
@@ -36,10 +29,8 @@ for i, x in enumerate(csv_data):
             status = 'SenLev3'
         else:
             status = ''
-            # print status
-        new_row.append(status)
-
+        row.append(status)
+        all.append(row)
+csv_write.writerows(all)
 data_file.close()
-
-
 data_write.close()
